1604536783f42dc45bd75116ec0a82ad20c22a84
carlota <carlota@gmail.com>
2022-12-22 23:49:10 +0100

    c'est fini

--- my_cp.c
+++ my_cp.c
@@ -5,30 +5,27 @@
 
 #define N 1024
 
-
-int file, desc1, desc2, dif;
+// INICIALITZACIÓ VARIABLES
+int file, desc1, desc2, bytes=1;
 char arxiu[N], contingut[N];
 
+// MAIN
 int main(){
-  scanf("%s",arxiu); //scanf--> escaneja el nom de l'arxiu que li posem (%c char) &-->pq es una varible
+  scanf("%s",arxiu); //scanf escaneja el nom de l'arxiu que li posem (%s indica un string) 
   desc1=open(arxiu, O_RDONLY); // nomes volem llegir l'arxiu, ja que hi farem una còpia, comanda per obrir el fitxer
+  
   if (desc1==-1){ // si al executar l'open dona -1 significa que hi ha un error a l'obrir-lo
     printf("Error: L'arxiu no existeix \n");
   }
 
   else{
-     read(desc1, contingut, N); //llegeix l'arxiu
-     desc2=open( "copia.fi", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU );  //creant el fitxer on copiarem l'arxiu 
-     write(desc2, contingut, N); // se suposa que escriu el contingut de l'arxiu al nou fitxer
-//     desc3=diff 'arxiu' 'copiafi';//per veure les diferències, serveix de comprovació
-//     if (desc3==-1){
-//       printf("Els arxius no són iguals.");
+    desc2=open( "copia.fi", O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU );  //creant el fitxer on copiarem l'arxiu 
+    while (bytes!=0){ //mentres que no arribi al final de l'arxiu continua llegint-lo:
+       bytes=read(desc1, contingut, N); //llegeix el contingut de l'arxiu
+     
+       write(desc2, contingut, bytes); // escriu el contingut de l'arxiu al nou fitxer creat anteriorment
      }
-//     else{
-//       printf("Els arxius són iguals.");
-//     }
-  close(desc1);
-  close(desc2);
-//  close(desc3);
-//  }
+  }
+  close(desc1); //tanquem l'arxiu introduit pel teclat
+  close(desc2); //tanquem l'arxiu creat
 }
